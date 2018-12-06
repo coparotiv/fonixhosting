a8f3280990466fdcd4d4aaf9ba92650d1b427568
coparotiv <44412380+coparotiv@users.noreply.github.com>
2018-12-06 21:49:11 +0300

    Обновление
    Обновление

---
+++ index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="ru">
+<head>
+<meta charset="utf-8">
+<title>FonixHosting - все необходимое для вашего сайта</title>
+<meta name="generator" content="FonixHosting Ltd">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="FonixHosting.css" rel="stylesheet">
+<link href="index.css" rel="stylesheet">
+<script src="jquery-1.11.3.min.js"></script>
+<script src="wb.carousel.min.js"></script>
+<script>
+$(document).ready(function()
+{
+   var Carousel1Opts =
+   {
+      delay: 3000,
+      duration: 500,
+      easing: 'linear',
+      mode: 'forward',
+      direction: '',
+      scalemode: 2,
+      pagination: true,
+      pagination_img_default: 'images/page_default.png',
+      pagination_img_active: 'images/page_active.png',
+      start: 0
+   };
+   $("#Carousel1").carousel(Carousel1Opts);
+   $("#Carousel1_back a").click(function()
+   {
+      $('#Carousel1').carousel('prev');
+   });
+   $("#Carousel1_next a").click(function()
+   {
+      $('#Carousel1').carousel('next');
+   });
+});
+</script>
+</head>
+<body>
+<div id="container">
+
+<div id="wb_Text6">
+<span style="color:#008000;font-family:'Trebuchet MS';font-size:29px;"><strong>Fonix Hosting</strong></span></div>
+<div id="wb_CssMenu1">
+<ul>
+<li class="firstmain"><a class="active" href="./index.php" target="_self" title="&#1043;&#1083;&#1072;&#1074;&#1085;&#1072;&#1103;">&#1043;&#1083;&#1072;&#1074;&#1085;&#1072;&#1103;</a>
+</li>
+</ul>
+</div>
+<div id="wb_Shape4">
+<img src="images/img0001.gif" id="Shape4" alt=""></div>
+<div id="wb_Carousel1">
+<div id="Carousel1">
+<div class="frame">
+<div id="wb_Text3">
+<div style="position:absolute;left:0px;top:0px;width:323px;height:29px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:24px;">Приветствуем вас!</span></div>
+<div style="position:absolute;left:0px;top:29px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;"><br></span></div>
+<div style="position:absolute;left:0px;top:47px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">Мы новый хостинг-провайдер на рынке, </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:65px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">предостовляющий услуги Виртуального хостинга, </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:83px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">VPS, Регистрацию доменных имен, Игрового </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:101px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">хостинга. Размещение ваших сайтов происходит в 3 </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:119px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">Дата центрах: Москва, Нидерланды, </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:137px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">Великобритания. Наши услуги предостовляются без </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:155px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">оверселлинга! Мы гарантируем качественную </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:173px;width:323px;height:29px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">работу сайтов и UP-TIME не ниже 99,95%</span></div>
+</div>
+<div id="wb_Image1">
+<img src="images/head1.jpg" id="Image1" alt=""></div>
+</div>
+<div class="frame">
+<div id="wb_Text1">
+<div style="position:absolute;left:0px;top:0px;width:323px;height:29px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:24px;">Размещение серверов</span></div>
+<div style="position:absolute;left:0px;top:29px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;"><br></span></div>
+<div style="position:absolute;left:0px;top:47px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">Мы размещаем сервера только в дата-центрах </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:65px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">сертификацией не ниже TIER 3. Это обеспечивает </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:83px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">надежное хранилище для вашего сайта. Все наши </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:101px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">сервера подключаются к 300 Мбит каналу связи. </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:119px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">Это значит, что сайты будут загружаться довольно </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:137px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">быстро. Мы не размещаем более 100 клиентов на </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:155px;width:323px;height:18px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">одном сервере, а стараемся разместить равномерно </span><span style="letter-spacing:323px;"> &nbsp; </span></div>
+<div style="position:absolute;left:0px;top:173px;width:323px;height:29px;"><span style="color:#FFFFFF;font-family:'Trebuchet MS';font-size:13px;">по кластеру!</span></div>
+</div>
+<div id="wb_Image2">
+<img src="images/DataPro.jpg" id="Image2" alt=""></div>
+</div>
+<div class="frame">
+</div>
+</div>
+<div id="Carousel1_back"><a style="cursor:pointer"><img alt="Back" style="border-width:0" src="images/carousel_back.png"></a></div>
+<div id="Carousel1_next"><a style="cursor:pointer"><img alt="Next" style="border-width:0" src="images/carousel_next.png"></a></div>
+</div>
+<div id="wb_CssMenu2">
+<ul>
+<li class="firstmain"><a href="#" target="_self" title="Home">Home</a>
+</li>
+<li><a href="#" target="_self" title="About Us">About&nbsp;Us</a>
+</li>
+<li><a href="#" target="_self" title="Services">Services</a>
+</li>
+<li><a href="#" target="_self" title="Solutions">Solutions</a>
+</li>
+<li><a href="#" target="_self" title="Support">Support</a>
+</li>
+<li><a href="#" target="_self" title="Contact">Contact</a>
+</li>
+</ul>
+</div>
+</div>
+<div id="Layer4">
+<div id="Layer4_Container">
+<div id="wb_Text11">
+<span style="color:#000000;font-family:'Trebuchet MS';font-size:11px;">Copyright © 2018 by &quot;FonixHosting&quot;&nbsp; ·&nbsp; All Rights reserved&nbsp; ·&nbsp; E-Mail: support@fonix-hosting.ru</span></div>
+</div>
+</div>
+</body>
+</html>
